87f7428ab07d0fcc51348a42b9acdf94b8b3bd73
Taylor Young <taylor@x-team.com>
2013-07-28 03:02:00 -0500

    a proper test, and patch version bump

--- test/out-expected/test.js
+++ test/out-expected/test.js
@@ -1,4 +1,7 @@
 ;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
-console.log('cheating');
-},{}]},{},[1])
+console.log('included');
+},{}],2:[function(require,module,exports){
+require('./included.js');
+console.log('main');
+},{"./included.js":1}]},{},[2])
 ;
